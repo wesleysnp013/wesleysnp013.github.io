b8ee90af90d37472e910601a3856463e1cdf694e
Wesley <48141765+wesleysnp013@users.noreply.github.com>
2025-04-16 22:22:45 -0300

    Ajustando função de calcular

--- index.js
+++ index.js
@@ -10,15 +10,6 @@ let operator = "";
 
 function handleClick(value) {
     const signal = ['-', '+', '*', '/'];  
-    if(value === "c" ) { 
-       
-        console.log(groupValue1.length)
-        memory = memory.slice(0, -1)
-        dataInput.value = memory;   
-
-        return
-    }
-
 
     if (!signal.includes(value)) {
         // Se não for um operador, concatena o número
@@ -28,30 +19,15 @@ function handleClick(value) {
 
     
         if(currentGroup === 2) {
-            // if(value === "c" ) { 
-       
-            //     memory = memory.slice(0, -1)
-            //     dataInput.value = memory;   
-            //     groupValue2.pop()
-        
-            //     return
-            // }
+         
             groupValue2.push(memory);
         }   
     } else {
-        // if(value === "c" ) { 
-       
-        //     memory = memory.slice(0, -1)
-        //     dataInput.value = memory;   
-        //     groupValue1.pop()
-    
-        //     return
-        // }
-        // Se for um operador, armazena o número no grupo atual
-        groupValue1.push(memory);  // Armazena o primeiro número
+      
+        groupValue1.push(memory); 
         currentGroup = 2; 
-        operator = value;  // Salva o operador
-        memory = "";  // Limpa a memória para o próximo número
+        operator = value;  
+        memory = ""; 
     };
 
 };
@@ -59,31 +35,14 @@ function handleClick(value) {
 function handleEquals() {
 
     if (groupValue1.length >= 0 && groupValue2.length >= 0 && operator !== "") {
-        console.log("teste ")
 
         let num1 = groupValue1.join('');  
-        let num2 = groupValue2.join(''); // Junta os valores do grupo 2
-        console.log("groupValue1", groupValue1)
-
-        console.log("groupValue2", parseFloat(num1) + parseFloat(num2) )
- 
+        let num2 = groupValue2.join('');
         // Usando eval() para calcular a expressão
-        let result = 0.0
+        let result =  eval(`${num1}  ${operator}  ${num2}` )
+ 
 
-        if (operator === '+') {
-            result = parseFloat(num1) + parseFloat(num2);
-        } else if (operator === '-') {
-            result = parseFloat(num1) - parseFloat(num2);
-        } else if (operator === '*') {
-            result = parseFloat(num1) * parseFloat(num2);
-        } else if (operator === '/') {
-            result = parseFloat(num1) / parseFloat(num2);
-        }
-
-        console.log(typeof result.toString())
-
-        console.log( result.toString())
-        dataInput.value = result.toString()
+        dataInput.value =  result
         // Limpa as variáveis para o próximo cálculo
         groupValue1 = [result.toString()]; // Guarda o resultado para continuar a operação
         groupValue2 = [];
